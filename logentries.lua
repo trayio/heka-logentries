ebbc496c201a05d44df10c50d120b7447aa84a87
Luka Furlan <luka@tray.io>
2016-03-18 18:29:37 +0000

    DockerLogInput

--- logentries.lua
+++ logentries.lua
@@ -7,37 +7,24 @@ containers = {}
 local token = read_config("token")
 
 function process_message()
-    local differentiator = read_message("Logger") -- sent by 'differentiator' in logentries.toml
-    local container_name = containers[differentiator]
 
-    local ok, payload = pcall(cjson.decode, read_message("Payload"))
-    if not ok then
-       return -1
-    end
+   local msg = decode_message(read_message("raw"))
+   local container_name = ""
 
-    if payload.Name then
-       containers[payload.ID] = payload.Name
-       return -2 -- no output, but not error (see https://hekad.readthedocs.org/en/latest/sandbox/index.html#functions-that-must-be-exposed-from-the-lua-sandbox)
-    end
+   for _, v in pairs(msg.Fields) do
+      if v.name == "ContainerName" then
+         container_name = v.value[1]
+         break
+      end
+   end
 
-    if not container_name then
-       container_name = "unknown_container"
-    elseif string.find(container_name, "RDAJR") then
-       -- local split = {}
-       -- for x in string.gmatch(container_name, "[^.]+") do
-       --    table.insert(split, x)
-       -- end
-       -- container_name = split[2]
-       return -2
-    elseif string.find(container_name, "/") then
-       container_name = string.gsub(container_name, "/", "", 1) -- docker prefixes container name with '/'
-       if container_name == "logentries" then
-          return -2
-       end
-    end
+   -- don't log connector logs
+   if string.find(container_name, "RDAJR") then
+      return -2
+   end
 
+   inject_message(token .. " " .. msg.Timestamp .. " name=" .. container_name .. " " .. msg.Hostname .. " " .. msg.Payload .. "\n")
 
-    inject_message(token .. " " .. payload.time .. " name=" .. container_name .. " " .. read_message("Hostname") .. " " .. payload.log)
+   return 0
 
-    return 0
 end
