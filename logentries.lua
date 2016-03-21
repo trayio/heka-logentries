819fe1ea14bf3b90cca038675eefb3e5310f2112
Luka Furlan <luka@tray.io>
2016-03-21 12:58:32 +0000

    fix timestamp and container name

--- logentries.lua
+++ logentries.lua
@@ -1,15 +1,17 @@
-require "cjson"
 require "string"
-require "table"
+require "os"
  
-containers = {}
-
 local token = read_config("token")
+
+-- time from DockerLogInput is in nanoseconds which Lua can't handle
+function date(timestamp)
+   return os.date("%Y-%m-%dT%H:%M:%SZ", string.format("%.0f", timestamp / 1000000000))
+end
 
 function process_message()
 
    local msg = decode_message(read_message("raw"))
-   local container_name = ""
+   local container_name = nil
 
    for _, v in pairs(msg.Fields) do
       if v.name == "ContainerName" then
@@ -18,16 +20,21 @@ function process_message()
       end
    end
 
-   if container_name == "" then
-      container_name = "unknown container"
+   if not container_name then
+      container_name = "unknown_container"
    end
-
+   
    -- don't log connector logs
    if string.find(container_name, "RDAJR") then
       return -2
    end
 
-   inject_message(token .. " " .. msg.Timestamp .. " name=" .. container_name .. " " .. msg.Hostname .. " " .. msg.Payload .. "\n")
+   local ok, timestamp = pcall(date, msg.Timestamp)
+   if not ok then
+      timestamp = os.date("%Y-%m-%dT%H:%M:%SZ")
+   end
+   
+   inject_message(token .. " " .. timestamp .. " name=" .. container_name .. " " .. msg.Hostname .. " " .. msg.Payload .. "\n")
 
    return 0
 
